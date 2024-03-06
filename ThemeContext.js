eadb180cf9a4acec809d91f6cd7ad845182b6f9b
Kondal Durgam <kondaldurgam733@gmail.com>
2024-03-06 16:35:40 +0530

    Create ThemeContext.js

---
+++ ThemeContext.js
@@ -0,0 +1,21 @@
+import React,{useContext,useState,createContext} from "react";
+
+const ThemeContext = createContext(undefined);
+
+
+
+export const ThemeProvider = ({children})=>{
+    const [theme,setTheme] = useState("light");
+
+    return(
+        <ThemeContext.Provider value={{
+            theme,
+            toggleTheme:()=> setTheme(theme==='light' ? 'dark' : 'light')}}>
+                {children}
+
+        </ThemeContext.Provider>
+    )
+};
+
+
+export const useTheme = ()=>useContext(ThemeContext);
